ee5a53af6fe87f24fe3c22cb139b5a13ec3caac0
dev-suleymanov <dev.suleymanov@gmail.com>
2024-03-19 13:44:23 +0300

    136. Single Number

---
+++ src/main/java/leetcode/java/bit_manipulation/BitManipulation.java
@@ -0,0 +1,21 @@
+package leetcode.java.bit_manipulation;
+
+/*
+    https://leetcode.com/problems/single-number/description/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+public class BitManipulation {
+    public static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int el : nums) {
+            result ^= el;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4, 1, 2, 1, 2};
+        int result = singleNumber(array);
+        System.out.println(result);
+    }
+}
